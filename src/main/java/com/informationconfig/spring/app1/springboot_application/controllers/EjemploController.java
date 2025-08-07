3b228992fb176c7c087307328126b011c255e58a
Santiago Mafla <santiago.mafla8@gmail.com>
2025-08-07 10:29:51 -0500

    Proyect video 7

--- src/main/java/com/informationconfig/spring/app1/springboot_application/controllers/EjemploController.java
+++ src/main/java/com/informationconfig/spring/app1/springboot_application/controllers/EjemploController.java
@@ -1,5 +1,6 @@
 package com.informationconfig.spring.app1.springboot_application.controllers;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -10,8 +11,10 @@ public class EjemploController {
     @GetMapping("/detalles_info")
 
     
-    public String info() {
+    public String info(Model model) {
+        model.addAttribute("Titulo", "Servidor en linea");
+        model.addAttribute("Servidor", "Informacion Configurada");
+        model.addAttribute("Version", "1.0.0");
         return "detalles_info";
     }
-
 }
