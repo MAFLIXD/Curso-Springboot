4f2087722aadabfdda762b72b7b532e3821a1499
Santiago Mafla <santiago.mafla8@gmail.com>
2025-08-08 19:49:31 -0500

    Proyect video 9

--- src/main/java/com/informationconfig/spring/app1/springboot_application/controllers/EjemploController.java
+++ src/main/java/com/informationconfig/spring/app1/springboot_application/controllers/EjemploController.java
@@ -2,19 +2,18 @@ package com.informationconfig.spring.app1.springboot_application.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
+import com.informationconfig.spring.app1.springboot_application.models.Empleados;
 
 @Controller
 public class EjemploController {
 
-    @GetMapping("/detalles_info")
+    @GetMapping("/detalles_info") //Maneja la solicitud HTTP
 
-    
-    public String info(Model model) {
-        model.addAttribute("Titulo", "Servidor en linea");
-        model.addAttribute("Servidor", "Informacion Configurada");
-        model.addAttribute("Version", "1.0.0");
+    public String info(Model model) { //Pasa los datos del controller a la vista
+
+        Empleados empleado1 = new Empleados("Mateo", "Perez", "Calle Falsa 123", "Desarrollador", 30, 123456789, 1);
+
+        model.addAttribute("Empleado", empleado1);
         return "detalles_info";
     }
 }
--- src/main/java/com/informationconfig/spring/app1/springboot_application/controllers/EjemploRestController.java
+++ src/main/java/com/informationconfig/spring/app1/springboot_application/controllers/EjemploRestController.java
@@ -3,6 +3,9 @@ import java.util.HashMap;
 import java.util.Map;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.informationconfig.spring.app1.springboot_application.models.Empleados;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -14,10 +17,12 @@ public class EjemploRestController {
     @GetMapping(path = "/detalles_info2")
     
     public  Map<String, Object> detalles_info2 () {
+
+        Empleados empleado1 = new Empleados("Mateo", "Perez", "Calle Falsa 123", "Desarrollador", 30, 123456789, 1);
+
         Map<String, Object> respuesta = new HashMap<>();
-        respuesta.put("Titulo", "Servidor en linea");
-        respuesta.put("Servidor", "Informacion Configurada");
-        respuesta.put("Version", "2.0.0");
+        respuesta.put("Empleado", "Datos Empleado");
+        respuesta.put("Informacion", empleado1); 
         return respuesta;
     }
 }
