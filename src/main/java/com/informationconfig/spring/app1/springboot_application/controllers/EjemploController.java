98767a9b90e0c0601f5152cd6d7ea68896fe9fc8
Santiago Mafla <santiago.mafla8@gmail.com>
2025-08-12 15:48:16 -0500

    Proyect video 14

--- src/main/java/com/informationconfig/spring/app1/springboot_application/controllers/EjemploController.java
+++ src/main/java/com/informationconfig/spring/app1/springboot_application/controllers/EjemploController.java
@@ -1,7 +1,10 @@
 package com.informationconfig.spring.app1.springboot_application.controllers;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import com.informationconfig.spring.app1.springboot_application.models.Empleados;
 
 @Controller
@@ -10,10 +13,18 @@ public class EjemploController {
     @GetMapping("/detalles_info") //Maneja la solicitud HTTP
 
     public String info(Model model) { //Pasa los datos del controller a la vista
-
         Empleados empleado1 = new Empleados("Mateo", "Perez", null, "Desarrollador", 30, 123456789, 1);
-
         model.addAttribute("Empleado", empleado1);
         return "detalles_info";
+    }
+
+    @ModelAttribute("Empleados")
+    public List<Empleados> getEmpleados() {
+        return Arrays.asList(
+            new Empleados("Mateo", "Perez", null, "Desarrollador", 30, 123456789, 1),
+            new Empleados("Ana", "Gomez", null, "Diseñadora", 28, 987654321, 2),
+            new Empleados("Luis", "Martinez", null, "Gerente", 35, 456789123, 3),
+            new Empleados("Sofia", "Lopez", null, "Analista", 32, 321654987, 4)
+        );
     }
 }
---
+++ src/main/java/com/informationconfig/spring/app1/springboot_application/controllers/ParametroDTO.java
@@ -0,0 +1,13 @@
+package com.informationconfig.spring.app1.springboot_application.controllers;
+
+public class ParametroDTO {
+    private String informacion;
+
+    public String getInformacion() {
+        return informacion;
+    }
+
+    public void setInformacion(String informacion) {
+        this.informacion = informacion;
+    }
+}
